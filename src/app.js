1712790336778cd4ec69f6fa1b95f42a8ac421fa
Allan Kildare <allankildare27@gmail.com>
2021-01-19 18:56:22 -0300

    + models

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const port = 3001
+
+app.use(bodyParser.json())
 
 const controllerUsuario = require('./controllers/usuario-controller')
 const controllerTarefa = require('./controllers/tarefa-controller')
--- src/controllers/tarefa-controller.js
+++ src/controllers/tarefa-controller.js
@@ -3,6 +3,7 @@ module.exports = app => {
         res.send('<h1>Tarefas</h1>')
     })
     app.post('/tarefas', (req, res) => {
+        console.log(`Corpo: ${req.body.nome}`)
         res.send('Rota POST de usuario ativada: tarefa adicionado ao banco de dados')
     })
 }
