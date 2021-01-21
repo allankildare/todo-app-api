9fe9aee3210c8cecd5a14fb1850deefa8dafcafb
Allan Kildare <allankildare27@gmail.com>
2021-01-21 01:56:22 -0300

    usuarios: post configurado

--- src/controllers/usuario-controller.js
+++ src/controllers/usuario-controller.js
@@ -1,15 +1,24 @@
+const bd = require('../infra/bd')
 const Usuario = require('../models/usuario')
 module.exports = (app, db) => {
     app.get('/usuarios', (req, res) => {
-        res.send('<h1>Usuarios</h1>')
+        res.send( req.body)
     })
 
-    
+    app.get('/usuarios/:email', (req, res) => {
+        for (let usr of bd.usuarios) {
+            if(usr.email == req.params._email) {
+                res.send(usr)
+            }
+        }
+        res.send(`Usuário não encontrado`)
+    })
+
     app.post('/usuarios', (req, res) => {
-        const user = new Usuario(req.body.nome, req.body.email, req.body.senha)
-        db.usuarios.push(user)
+        res.send(`Usuário ${req.body.email} adicionado(a) com sucesso`)
+        const usr = new Usuario(req.body.nome, req.body.email, req.body.senha)
+        db.usuarios.push(usr)
         console.log(db)
-        res.send('Ok')
     })
     
 }
